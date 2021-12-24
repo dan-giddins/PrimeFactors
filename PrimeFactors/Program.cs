f26babc7a8760429fa5d8a5a3dc70bbf76337d37
Dan <daniel.giddins112@gmail.com>
2021-12-24 11:06:07 +0000

    Add project files.

---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeFactors
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (var n = 0; n < 100; n++)
+			{
+				Console.Write($"{n} = ");
+				var p = 2;
+				var m = n;
+				while (m >= p * p)
+				{
+					if (m % p == 0)
+					{
+						Console.Write($"{p} * ");
+						m /= p;
+					}
+					else
+					{
+						p++;
+					}
+				}
+				Console.WriteLine($"{m}");
+			}
+		}
+	}
+}
