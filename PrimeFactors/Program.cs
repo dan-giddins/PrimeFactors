babd3674dc7499b4e261c60011203cbc50e2892c
Dan <daniel.giddins112@gmail.com>
2021-12-25 00:25:41 +0000

    add timing

--- PrimeFactors/Program.cs
+++ PrimeFactors/Program.cs
@@ -6,11 +6,16 @@ namespace PrimeFactors
 {
 	class Program
 	{
+		private const int Range = 100000;
+		private static System.Diagnostics.Stopwatch Watch;
+
 		static void Main()
 		{
+			// current method
+			Watch = System.Diagnostics.Stopwatch.StartNew();
 			// number, prime, power
-			var primeDictProc = new Dictionary<int, Dictionary<int, int>>();
-			for (var n = 2; n < 100; n++)
+			var primeDictCurrent = new Dictionary<int, Dictionary<int, int>>();
+			for (var n = 2; n < Range; n++)
 			{
 				var primeFactors = new List<int>();
 				var p = 2;
@@ -28,107 +33,67 @@ namespace PrimeFactors
 					}
 				}
 				primeFactors.Add(m);
-				primeDictProc.Add(n, primeFactors.GroupBy(x => x)
+				primeDictCurrent.Add(n, primeFactors.GroupBy(x => x)
 					.Select(x => new KeyValuePair<int, int>(x.Key, x.Count()))
 					.ToDictionary(x => x.Key, x => x.Value));
-				//Console.WriteLine($"{n} = {string.Join(" * ", primeDictProc[n].Select(x => $"{x.Key}^{x.Value}"))}");
 			}
-			//var pow2Count = primeDict.SelectMany(x => x.Value.Where(x => x.Key == 2).Select(x => x.Value));
-			//foreach (var item in pow2Count)
-			//{
-			//	Console.Write($"{item} ");
-			//}
-			//Console.WriteLine();
-			//Console.WriteLine();
-			//// every other power is 1
-			//var pow2CountRemove1 = pow2Count.Where(x => x != 1);
-			//foreach (var item in pow2CountRemove1)
-			//{
-			//	Console.Write($"{item} ");
-			//}
-			//Console.WriteLine();
-			//Console.WriteLine();
-			//// every other power from the remaining list is 2
-			//var pow2CountRemove2 = pow2CountRemove1.Where(x => x != 2);
-			//foreach (var item in pow2CountRemove2)
-			//{
-			//	Console.Write($"{item} ");
-			//}
-			//Console.WriteLine();
+			Watch.Stop();
+			//PrintPrimeDict(primeDictCurrent);
+			Console.WriteLine($"For a range of {Range}:");
+			var timeCurrent = Watch.ElapsedMilliseconds;
+			Console.WriteLine($"The current method took {timeCurrent} ms");
+
+			// fast? method
+			Watch = System.Diagnostics.Stopwatch.StartNew();
 			// number, prime, power
 			var primeDictFast = new Dictionary<int, Dictionary<int, int>>();
-			for (var n = 2; n < 100; n++)
+			for (var n = 2; n < Range; n++)
 			{
 				primeDictFast.Add(n, new Dictionary<int, int>());
 			}
-			//for (int prime = 2; prime < 100; prime++)
-			//{
-			//	for (int pow = 1; pow < Math.Log(100, prime); pow++)
-			//	{
-			//		for (var n = (int)Math.Pow(prime, pow); n < 100; n += (int)Math.Pow(prime, pow + 1))
-			//		{
-			//			primeDictFast[n].Add(prime, pow);
-			//		}
-			//	}
-			//}
-			//// 2s
-			//for (var pow = 1; pow < Math.Log(100, 2); pow++)
-			//{
-			//	// fact - 1 loops
-			//	for (var n = (int)Math.Pow(2, pow); n < 100; n += (int)Math.Pow(2, pow + 1))
-			//	{
-			//		primeDictFast[n].Add(2, pow);
-			//	}
-			//}
-			//// 3s
-			//for (var pow = 1; pow < Math.Log(100, 3); pow++)
-			//{
-			//	// fact - 1 loops
-			//	for (var n = (int)Math.Pow(3, pow); n < 100; n += (int)Math.Pow(3, pow + 1))
-			//	{
-			//		primeDictFast[n].Add(3, pow);
-			//	}
-			//	for (var n = (int)Math.Pow(3, pow) + (int)Math.Pow(3, pow); n < 100; n += (int)Math.Pow(3, pow + 1))
-			//	{
-			//		primeDictFast[n].Add(3, pow);
-			//	}
-			//}
-			//// 5s
-			//for (var pow = 1; pow < Math.Log(100, 5); pow++)
-			//{
-			//	// fact - 1 loops
-			//	for (var l = 0; l < 5 - 1; l++)
-			//	{
-			//		for (var n = (int)Math.Pow(5, pow) + l * (int)Math.Pow(5, pow); n < 100; n += (int)Math.Pow(5, pow + 1))
-			//		{
-			//			primeDictFast[n].Add(5, pow);
-			//		}
-			//	}
-			//}
 			// loop through potential primes
-			for (var potPrime = 2; potPrime < 100; potPrime++)
+			for (var potPrime = 2; potPrime < Range; potPrime++)
 			{
+				var isPrime = true;
 				// loop through the possible powers for said potPrime
-				for (var pow = 1; pow < Math.Log(100, potPrime); pow++)
-				{ 
-					// how many extra
-					for (var loop = 0; loop < potPrime - 1; loop++)
+				for (var pow = 1; isPrime && pow < Math.Log(Range, potPrime); pow++)
+				{
+					// sequence modifer thing(based on potPrime)
+					for (var loop = 0; isPrime && loop < potPrime - 1; loop++)
 					{
+						// sequence
 						for (
 							var n = (int)Math.Pow(potPrime, pow) + loop * (int)Math.Pow(potPrime, pow);
-							n < 100;
+							isPrime && n < Range;
 							n += (int)Math.Pow(potPrime, pow + 1))
 						{
 							// is prime?
-							if (primeDictFast[2].Aggregate(1, (acc, val) => acc * val))
-							primeDictFast[n].Add(potPrime, pow);
+							if (primeDictFast[n].Aggregate(1, (acc, val) =>
+								acc * (int)Math.Pow(val.Key, val.Value)) < n)
+							{
+								primeDictFast[n].Add(potPrime, pow);
+							}
+							else
+							{
+								// current number is not prime
+								isPrime = false;
+							}
 						}
 					}
 				}
 			}
-			for (var n = 2; n < 100; n++)
+			Watch.Stop();
+			//PrintPrimeDict(primeDictFast);
+			var timeFast = Watch.ElapsedMilliseconds;
+			Console.WriteLine($"The fast method took {timeFast} ms");
+			Console.WriteLine($"The fast method is {timeCurrent - timeFast} ms faster!");
+		}
+
+		private static void PrintPrimeDict(Dictionary<int, Dictionary<int, int>> primeDict)
+		{
+			for (var n = 2; n < Range; n++)
 			{
-				Console.WriteLine($"{n} = {string.Join(" * ", primeDictFast[n].Select(x => $"{x.Key}^{x.Value}"))}");
+				Console.WriteLine($"{n} = {string.Join(" * ", primeDict[n].Select(x => $"{x.Key}^{x.Value}"))}");
 			}
 		}
 	}
